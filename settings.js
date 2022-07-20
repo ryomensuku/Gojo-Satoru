84592b5dfc263cf0e77ece3fa46188dd64980fda
ryomensuku <109031092+ryomensuku@users.noreply.github.com>
2022-07-20 22:51:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['9165865850']
+global.premium = ['9165865850']
+global.ownernomer = 9165865850
+global.ownername = '🐦Star Ryomen sukuna 
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
